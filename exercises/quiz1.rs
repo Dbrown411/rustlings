4edca1b87050b664e5e4d32d3644ec103d396982
Dillon Brown <Dillon411@gmail.com>
2023-03-11 21:34:34 -0500

    completed

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,19 +10,20 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
+fn calculate_price_of_apples(num: u32) -> u32 {
+    if num <= 40 {
+        return num * 2;
+    } else {
+        return num;
+    }
+}
 
-// Put your function here!
-// fn calculate_price_of_apples {
-
-// Don't modify this function!
 #[test]
 fn verify_test() {
     let price1 = calculate_price_of_apples(35);
     let price2 = calculate_price_of_apples(40);
     let price3 = calculate_price_of_apples(41);
     let price4 = calculate_price_of_apples(65);
-
     assert_eq!(70, price1);
     assert_eq!(80, price2);
     assert_eq!(41, price3);
